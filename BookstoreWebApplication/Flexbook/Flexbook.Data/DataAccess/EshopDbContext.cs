c8b394bbd5647c67801464cc4e928913bd0bfdd1
Lefterisk0025 <43676375+Lefterisk0025@users.noreply.github.com>
2022-05-28 15:51:18 +0300

    Initial project deleted...a new era begins

--- BookstoreWebApplication/Flexbook/Flexbook.Data/DataAccess/EshopDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Flexbook.Data.DataAccess
-{
-    public class EshopDbContext : DbContext
-    {
-        public EshopDbContext(DbContextOptions options) : base(options) { }
-    }
-}
