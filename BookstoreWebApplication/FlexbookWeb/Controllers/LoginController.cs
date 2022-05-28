b5d381dc9891121b9a0372d065d205a8de9a8b7a
Lefterisk0025 <43676375+Lefterisk0025@users.noreply.github.com>
2022-05-28 18:19:45 +0300

    Login page created

---
+++ BookstoreWebApplication/FlexbookWeb/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlexbookWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
