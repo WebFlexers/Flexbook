753d311fb212d702488190c1af2bf0d7515c9e27
StyleM <57811193+mStylias@users.noreply.github.com>
2022-06-27 11:38:03 +0300

    Remove old asp.net project

--- BookstoreWebApplication/BookstoreWebApp/BookstoreWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookstoreWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
