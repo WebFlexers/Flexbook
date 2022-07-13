05b69f26f771495aee527c1c88cc406ae1f06f77
Lefterisk0025 <43676375+Lefterisk0025@users.noreply.github.com>
2022-07-13 18:15:13 +0300

    Statistics Controller and Service added

---
+++ Flexbook/Flexbook.Services/Statistics/IStatisticsService.cs
@@ -0,0 +1,15 @@
+﻿using Flexbook.Data.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flexbook.Services.Statistics;
+
+public interface IStatisticsService
+{
+    public List<Data.Models.OrderSystem.Order> GetAllOrdersByAuthorId(int author_id);
+
+    public Book GetBestSellerBookOfAuthor(int author_id);
+}
---
+++ Flexbook/Flexbook.Web/Controllers/StatisticsController.cs
@@ -0,0 +1,29 @@
+﻿using Flexbook.Services.Statistics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flexbook.Web.Controllers;
+
+[Route("api/author/statistics")]
+[ApiController]
+public class StatisticsController : Controller
+{
+    private readonly ILogger<CustomerController> _logger;
+
+    private IStatisticsService _statisticsService;
+
+    public StatisticsController(ILogger<CustomerController> logger, IStatisticsService statisticsService)
+    {
+        _logger = logger;
+
+        _statisticsService = statisticsService;
+    }
+
+    [HttpGet("get_all_orders")]
+    public IActionResult GetAllOrdersByAuthor(int author_id)
+    {
+        var tempOrders = _statisticsService.GetAllOrdersByAuthorId(author_id);
+
+        return Ok(tempOrders);
+    }
+
+}
